fde610bab2ebdf0092c1a85ba0bd1ef987fd94c3
23f3003750 <23f3003750@ds.study.iitm.ac.in>
2026-07-11 22:34:48 +0530

    fix: minor change in base model

--- backend/app/models/base_model.py
+++ backend/app/models/base_model.py
@@ -8,7 +8,7 @@ IST = pytz.timezone("Asia/Kolkata")
 class BaseModel(db.Model):
     __abstract__ =True
 
-    created_at=db.Column(db.DateTime,default=lambda:datetime.now(IST),nullable=False)
-    updated_at=db.Column(db.DateTime,default=lambda:datetime.now(IST),nullable=False)
+    created_at=db.Column(db.DateTime(timezone=True),default=lambda:datetime.now(IST),nullable=False)
+    updated_at=db.Column(db.DateTime(timezone=True),default=lambda:datetime.now(IST),nullable=False)
     deleted_at=db.Column(db.DateTime)
 
