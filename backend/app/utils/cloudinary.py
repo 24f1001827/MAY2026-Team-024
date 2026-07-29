b693ef2714457a811136d780b91fdcaceb2ba413
23f3003750 <23f3003750@ds.study.iitm.ac.in>
2026-07-29 14:33:31 +0530

    feat(upload): add Cloudinary document upload support

--- backend/app/utils/cloudinary.py
+++ backend/app/utils/cloudinary.py
@@ -16,6 +16,20 @@ def upload_image(image):
         "public_id": response["public_id"],
     }
 
+def upload_document(document,folder):
+    """
+    Upload document to cloudinary.
+    """
+    response = cloudinary.uploader.upload(
+        document,
+        folder=folder
+    )
+
+    return {
+        "document_url":response["secure_url"],
+        "public_id":response["public_id"]
+    }
+
 def delete_image(public_id):
     """
     Delete image from Cloudinary.
