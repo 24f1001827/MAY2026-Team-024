01a19e8bb49be036e4bf095b958c82ce74d93616
23f3003750 <23f3003750@ds.study.iitm.ac.in>
2026-07-29 22:51:34 +0530

    feat(work_order): implement work order schema to validate request and response

---
+++ backend/app/schemas/work_order_schema.py
@@ -0,0 +1,27 @@
+from marshmallow import Schema, fields, validate
+from app.models import WorkOrderStatus
+
+
+class CreateWorkOrderSchema(Schema):
+    scope_of_work = fields.String(
+        required=True,
+        validate=validate.Length(
+            min=10,
+            max=2000,
+        ),
+    )
+
+    remarks = fields.String(
+        required=False,
+        allow_none=True,
+        validate=validate.Length(
+            max=1000,
+        ),
+    )
+
+class UpdateWorkOrderStatusSchema(Schema):
+    status = fields.Enum(
+        WorkOrderStatus,
+        required=True,
+        by_value=True,
+    )
