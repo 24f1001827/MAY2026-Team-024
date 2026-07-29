a57782092e90f95bb4f5b1589d682cd83e2fd203
23f3003750 <23f3003750@ds.study.iitm.ac.in>
2026-07-29 14:39:35 +0530

    fix: minor change in officer service to change the complaint status

--- backend/app/services/officer_service.py
+++ backend/app/services/officer_service.py
@@ -404,6 +404,7 @@ class OfficerService:
                 proposal.id,
             )
             proposal.agency.current_projects += 1
+            proposal.tender.complaint.status = ComplaintStatus.TENDER_ALLOTED
 
         proposal.status = new_status
 
