28f2178a4d147aeb9ad94fe23745f16e466e0309
23f3003750 <23f3003750@ds.study.iitm.ac.in>
2026-07-30 22:02:54 +0530

    feat(notifications): implement notifications routes to create notification and mark as read

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -4,3 +4,4 @@ from .admin_complaint_route import admin_complaint_bp
 from .admin_user_route import admin_user_bp
 from .officer_route import officer_bp
 from .agency_route import agency_bp
+from .notification_route import notification_bp
