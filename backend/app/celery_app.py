8c21f731cc5dc61589c8d7367d5e1d8c925b6840
23f3003750 <23f3003750@ds.study.iitm.ac.in>
2026-08-12 20:48:49 +0530

    fix(celery): register complaint auto-close celery task

--- backend/app/celery_app.py
+++ backend/app/celery_app.py
@@ -1,23 +1,34 @@
 from celery import Celery, Task
 
 
-def celery_init_app(app):
+def celery_init_app(flask_app):
+
     class FlaskTask(Task):
+
         def __call__(self, *args, **kwargs):
-            with app.app_context():
+
+            with flask_app.app_context():
                 return self.run(*args, **kwargs)
 
-    celery = Celery(app.import_name)
+    celery = Celery(
+        flask_app.import_name
+    )
 
-    celery.config_from_object(app.config["CELERY"])
+    celery.config_from_object(
+        flask_app.config["CELERY"]
+    )
 
     celery.Task = FlaskTask
 
     celery.set_default()
 
-    app.extensions["celery"] = celery
-    
-    celery.autodiscover_tasks(["app.tasks"])
+    flask_app.extensions["celery"] = celery
 
+    celery.autodiscover_tasks(
+        ["app.tasks"]
+    )
+
+    import app.tasks.email_task
+    import app.tasks.complaint_task
 
     return celery
